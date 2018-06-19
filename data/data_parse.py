d00ca7308234ead17f22c0e768f281b07b371d93
will12525 <william12525@gmail.com>
2018-06-19 15:52:04 -0400

    item data parsed

---
+++ data/data_parse.py
@@ -0,0 +1,59 @@
+import json
+import io
+from pprint import pprint
+try:
+    to_unicode = unicode
+except NameError:
+    to_unicode = str
+
+with open('item_data.json') as f:
+    data = json.load(f)
+common = dict()
+uncommon = dict()
+rare = dict()
+vvrare = dict()
+legendary = dict()
+
+
+with io.open("test_data.json", 'w', encoding='utf8') as outfile:
+    ccount = 0;
+    ucount = 0;
+    rcount = 0;
+    vcount = 0;
+    lcount = 0;
+    for i in data:
+        rarity = data[i]["rarity"]
+        if rarity == 'Common':
+            common.update({ccount : data[i]})
+            ccount+=1
+        elif rarity == 'Uncommon':
+            uncommon.update({ucount : data[i]})
+            ucount+=1
+        elif rarity == 'Rare':
+            rare.update({rcount : data[i]})
+            rcount+=1
+        elif rarity == 'Very Rare':
+            vvrare.update({vcount : data[i]})
+            vcount+=1
+        elif rarity == 'Legendary':
+            legendary.update({lcount : data[i]})
+            lcount+=1
+
+
+    output = {  '0' : common,
+                '1' : uncommon,
+                '2' : rare,
+                '3' : vvrare,
+                '4' : legendary}
+
+    str_ = json.dumps(
+        output,
+        indent=4,
+        separators=(',', ': '),
+        ensure_ascii=False)
+    outfile.write(to_unicode(str_))
+            
+        
+
+
+
