015bd72fe0315a77b8672921944c611ea2d34e03
Tom <tom.plano10@gmail.com>
2018-07-10 11:56:16 -0400

    maybe i messed up a datafile

--- factories/characterFactory.cpp
+++ factories/characterFactory.cpp
@@ -1,9 +1,33 @@
 #include "characterFactory.h"
 
-CharacterFactory::CharacterFactory(){}
+CharacterFactory::CharacterFactory()
+{
+  rng.seed(std::random_device()());
+
+  std::cout<<"Here\n";
+  std::ifstream file1("data/races.json");
+  file1 >> race_root;
+  std::cout<<"Here1.5\n";
+
+  std::ifstream file2("data/classes.json");
+  file2 >> class_root;
+  std::cout<<"Here2\n";
+
+}
 
 Character CharacterFactory::create_character()
 {
+  int num_races = race_root["race"].size()-1;
+  std::uniform_int_distribution<int> race_select(0,num_races);
+
+  int num_classes = class_root["class"].size()-1;
+  std::uniform_int_distribution<int> class_select(0,num_classes);
+
+  Json::Value char_race = race_root["race"][race_select(rng)];
+  Json::Value char_class = class_root["class"][class_select(rng)];
+
+  std::cout<< char_race["name"].asString()<< " "<< char_class["name"].asString()<<std::endl;
+
     Character ch;
     return ch;
 }
--- factories/characterFactory.h
+++ factories/characterFactory.h
@@ -1,7 +1,10 @@
 #ifndef CHARACTER_FACTORY
 #define CHARACTER_FACTORY
+#include <random>
+#include <fstream>
 #include "../products/character.h"
 #include "../jsoncpp/dist/json/json.h"
+#include <iostream>
 
 
 class CharacterFactory
@@ -10,6 +13,12 @@ public:
 CharacterFactory();
 
 Character create_character();
+
+private:
+  std::default_random_engine rng;
+  Json::Value class_root;
+  Json::Value race_root;
+
 
 };
 #endif
